dcc1060203b50d34278657621b396abe04a601d9
Mikhail Faraponov <mikhail.faraponov@sigma.software>
2018-02-23 16:55:07 +0200

    First step

---
+++ cmd/root.go
@@ -0,0 +1,16 @@
+package cmd
+
+import "fmt"
+import "github.com/hashicorp/raft"
+import "github.com/hashicorp/memberlist"
+import "github.com/hashicorp/serf"
+import "google.golang.org/grpc"
+import "golang.org/x/crypto/ssh"
+import "github.com/Shopify/sarama"
+import "github.com/streadway/amqp"
+import "github.com/libvirt/libvirt-go"
+import "github.com/digitalocean/go-qemu"
+
+func Execute() {
+    fmt.Println("Fuck")
+}
