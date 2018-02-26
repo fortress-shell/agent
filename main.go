a48712f2d52594a212c724987fc5f0ed1bbf9de4
Mikhail Faraponov <mikefaraponov@fortress-1.local>
2018-02-27 00:57:45 +0200

    Updated

--- main.go
+++ main.go
@@ -1,14 +1,38 @@
-package cmd
+package main
 
-import "fmt"
-import "github.com/hashicorp/memberlist"
-import "github.com/hashicorp/serf"
-import "google.golang.org/grpc"
-import "golang.org/x/crypto/ssh"
-import "github.com/Shopify/sarama"
-import "github.com/libvirt/libvirt-go"
-import "github.com/digitalocean/go-qemu"
+import (
+  "os"
+  "fmt"
+  "strings"
+  "github.com/Shopify/sarama"
+)
+// import "github.com/hashicorp/memberlist"
+// import "github.com/hashicorp/serf"
+// import "google.golang.org/grpc"
+// import "golang.org/x/crypto/ssh"
+// import "
+// import "github.com/libvirt/libvirt-go"
+// import "github.com/digitalocean/go-qemu"
 
-func Execute() {
-    fmt.Println("Fuck")
+func main() {
+  kafkaUrl := os.Getenv("KAFKA_URL")
+  brokerList := strings.Split(kafkaUrl, ",")
+  config := sarama.NewConfig()
+  config.Producer.RequiredAcks = sarama.WaitForAll
+  config.Producer.Retry.Max = 10
+  config.Producer.Return.Successes = true
+  producer, err := sarama.NewSyncProducer(brokerList, config)
+  if err != nil {
+    fmt.Println("Failed to start Sarama producer:", err)
+  }
+  defer producer.Close()
+  msg := sarama.ProducerMessage{
+    Topic: "dc1",
+    Key: sarama.StringEncoder("main"),
+    Value: sarama.StringEncoder("top error fuck message"),
+  }
+  for {
+    partition, offset, err := producer.SendMessage(&msg)
+    fmt.Println("fuck", partition, offset, err)
+  }
 }
